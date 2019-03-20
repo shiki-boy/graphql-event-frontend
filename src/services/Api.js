e6b1d248f58b205bcd592583d1504367b71ad296
Raghav Kaushik <raghav.kaushik611@gmail.com>
2019-03-20 20:51:05 +0530

    a

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,21 +1,18 @@
-try {
   import axios from 'axios'
 
-  if (!process.env.DEPLOYED) {
+  console.log(window.location.host)
+  if (!window.location.host.includes('.com')) {
     export default () => {
       return axios.create({
         baseURL: 'http://localhost:3000',
         // withCredentials: true         // ! to set cookies from request calls (allow)
       })
     }
+  } else {
+    export default () => {
+      return axios.create({
+        baseURL: process.env.BASE_URL,
+        // withCredentials: true         // ! to set cookies from request calls (allow)
+      })
+    }
   }
-
-} catch (error) {
-  if (process.env.DEPLOYED)
-  export default () => {
-    return axios.create({
-      baseURL: process.env.BASE_URL,
-      // withCredentials: true         // ! to set cookies from request calls (allow)
-    })
-  }
-}
